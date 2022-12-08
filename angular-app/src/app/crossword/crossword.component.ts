9e2e310fb7d7ae1c24409e5c1532718be74b37d4
erickubenka <code@code-fever.de>
2022-12-08 08:50:38 +0100

    Added game administration for "Buchstabensalat"

--- angular-app/src/app/crossword/crossword.component.ts
+++ angular-app/src/app/crossword/crossword.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-crossword',
   templateUrl: './crossword.component.html',
-  styleUrls: ['./crossword.component.css']
+  styleUrls: ['./crossword.component.scss']
 })
 export class CrosswordComponent {
 
+  crossword = new Crossword();
+  gamesWon = 0;
+  timer = 60;
+  isStarted = false;
+
+  solve(word: string) {
+    if (!this.crossword.solvedWords.includes(word)) {
+      this.crossword.solvedWords.push(word);
+    }
+
+    if (this.crossword.solvedWords.length == this.crossword.words.length) {
+      this.crossword = new Crossword();
+      this.gamesWon++;
+    }
+  }
+
+  start() {
+    this.isStarted = true;
+    let timerInterval = setInterval(() => {
+      this.timer = this.timer - 1;
+      if (this.timer <= 0) {
+        clearInterval(timerInterval);
+
+      }
+    }, 1000);
+  }
+}
+
+export class Crossword {
+
+  // 5x5
+  public rows: string[] = [
+    "TLSAE",
+    "AODEB",
+    "ABTDA",
+    "GBUAL",
+    "PTSAL",
+  ];
+
+  public words: string[] = ["total", "last", "ball"];
+  public solvedWords: string[] = [];
 }
